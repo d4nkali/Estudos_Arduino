947ceb8bc281ee312dc402ade4b12962615ec0e4
d4nkali <133923419+d4nkali@users.noreply.github.com>
2023-10-31 20:50:29 -0300

    Adicionando o teste da biblioteca brasilino

---
+++ Arduino/teste_brasilino/teste_brasilino.ino
@@ -0,0 +1,24 @@
+/*
+
+ Teste da biblioteca brasilino
+
+*/
+
+// Inclusão da biblioteca
+
+ #include <Brasilino.h>
+
+funcao configurar() { 
+
+  saida(13); // Configura o pino 13 como saida
+
+}
+
+funcao repetir() {
+  
+ ligar(13) // Liga o pino
+ esperar(1) // Espera 1 segundo
+ desligar(13) // Desliga o pino
+ esperar(1) // Espera 1 segundo
+
+}
